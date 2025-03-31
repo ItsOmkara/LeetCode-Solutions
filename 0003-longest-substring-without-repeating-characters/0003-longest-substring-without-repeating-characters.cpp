f122b6fc9f41eb49c47152bad06d4a0f1a861b6b
ItsOmkara <141958946+ItsOmkara@users.noreply.github.com>
2025-04-01 05:06:49 +0530

    Update 0003-longest-substring-without-repeating-characters.cpp

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-     int freq[256] = {0};  
+    int freq[256] = {0};  
     int left = 0, maxLength = 0;
 
     for (int right = 0; right < s.length(); right++) {
