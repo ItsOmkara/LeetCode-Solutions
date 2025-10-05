2f80943ce0d1fef250407e2b8f5a51ade0bca250
Omkar Birajdar <141958946+ItsOmkara@users.noreply.github.com>
2025-10-05 13:56:24 +0530

    Time: 1 ms (77.35%), Space: 41.4 MB (50.13%) - LeetHub

--- 2547-odd-string-difference/2547-odd-string-difference.java
+++ 2547-odd-string-difference/2547-odd-string-difference.java
@@ -1,31 +1,35 @@
 class Solution {
     public String oddString(String[] words) {
-       
-        String first = diff(words[0]);
-        String second = diff(words[1]);
-        String third = diff(words[2]);
+        int n = words[0].length();
 
-        String common;
-        if (first.equals(second) || first.equals(third)) {
-            common = first;
-        } else {
-            common = second;
-        }
-
-        
-        for (String w : words) {
-            if (!diff(w).equals(common)) {
-                return w;
+        // Generate difference arrays
+        int[][] diffArrays = new int[words.length][n - 1];
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                diffArrays[i][j] = words[i].charAt(j + 1) - words[i].charAt(j);
             }
         }
-        return "";
+
+        // Compare difference arrays to find the odd one
+        for (int i = 0; i < words.length; i++) {
+            boolean isOdd = true;
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && areArraysEqual(diffArrays[i], diffArrays[j])) {
+                    isOdd = false;
+                    break;
+                }
+            }
+            if (isOdd) return words[i];
+        }
+
+        return ""; // Default case
     }
 
-    private String diff(String s) {
-        String ans = "";
-        for (int i = 0; i < s.length() - 1; i++) {
-            ans += (s.charAt(i+1) - s.charAt(i)) + ",";
+    private boolean areArraysEqual(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) return false;
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) return false;
         }
-        return ans;
+        return true;
     }
 }
